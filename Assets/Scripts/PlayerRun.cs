b483de03c687c4c98f51d415c788ad98e57aa5be
Meelis <meelis.perli@gmail.com>
2019-10-05 11:47:37 +0300

    Stuff

---
+++ Assets/Scripts/PlayerRun.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRun : MonoBehaviour
+{
+    public float speed;
+    public float rotationSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 moveVec = new Vector3();
+        float deltaSpeed = speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.W)){
+            moveVec.x += (Mathf.Cos(transform.rotation.z)*deltaSpeed);
+            moveVec.y += (Mathf.Sin(transform.rotation.z)*deltaSpeed);
+        } else if (Input.GetKey(KeyCode.S)){
+            moveVec.x += (Mathf.Cos(transform.rotation.z)*-deltaSpeed);
+            moveVec.y += (Mathf.Sin(transform.rotation.z)*-deltaSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.D)){
+            transform.Rotate(new Vector3(0,0,1), rotationSpeed*Time.deltaTime);
+        } else if (Input.GetKey(KeyCode.A)){
+            transform.Rotate(new Vector3(0,0,1), rotationSpeed*Time.deltaTime);
+        }
+    }
+}
