b26844f2b1cfe996b3fbf30b8617017d5eb598c9
Liibert Paul <oliibert@hotmail.com>
2019-10-06 19:46:19 +0300

    e

--- Assets/Scripts/Upgrades.cs
+++ Assets/Scripts/Upgrades.cs
@@ -55,7 +55,7 @@ public class Upgrades : MonoBehaviour
         if (update)
         {
             float[] p = points[Random.Range(0,5)];
-            if (currentShoeTier < 5)
+            if (currentShoeTier < 6)
             {
                 update = false;
                 shoesPrefab.transform.position= new Vector3(p[0],p[1],0);
