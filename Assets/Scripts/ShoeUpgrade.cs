38791774dccc7bbdaa191ec7855106f5dc6e24c8
Meelis <meelis.perli@gmail.com>
2019-10-06 19:37:21 +0300

    anim

--- Assets/Scripts/ShoeUpgrade.cs
+++ Assets/Scripts/ShoeUpgrade.cs
@@ -6,11 +6,14 @@ public class ShoeUpgrade : MonoBehaviour
 {
     public int preReqLevel;
 
+    private bool taken = false;
+
     private void OnTriggerEnter2D(Collider2D other) {
-        if (other.tag == "Player"){
+        if (other.tag == "Player" && !taken){
             other.GetComponent<Upgrades>().UpgradesShoes();
             GetComponent<Animator>().SetTrigger("Open");
             Destroy(this.gameObject, 3);
+            taken = true;
         } 
     }
 }
